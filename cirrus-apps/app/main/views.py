5acbad2ce89cfb604eb204102cb4e89823313b03
Nick Cote <ncote@ucar.edu>
2025-11-20 09:59:42 -0700

    Add in user apps

--- cirrus-apps/app/main/views.py
+++ cirrus-apps/app/main/views.py
@@ -1,9 +1,16 @@
 from flask import render_template, request, session, flash, send_from_directory
 from app import app
+import yaml
+import os
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    # Load applications from YAML file
+    apps_file = os.path.join(app.root_path, 'apps.yaml')
+    with open(apps_file, 'r') as f:
+        applications = yaml.safe_load(f)
+    
+    return render_template('home.html', applications=applications)
 
 @app.route('/templates/navbar.html')
 def navbar():

6c7777506ada48c1c9af8d4fa0de9123877c36c0
Nick Cote <ncote@ucar.edu>
2025-11-24 16:11:31 -0700

    Try another status approach

--- cirrus-apps/app/main/views.py
+++ cirrus-apps/app/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template, request, session, flash, send_from_directory
 from app import app
 import yaml
 import os
+import requests
+import datetime
 
 @app.route('/')
 def home():
@@ -9,7 +11,6 @@ def home():
 
 @app.route('/apps')
 def apps():
-    # Load applications from YAML file
     apps_file = os.path.join(app.root_path, 'apps.yaml')
     with open(apps_file, 'r') as f:
         applications = yaml.safe_load(f)
@@ -21,6 +22,28 @@ def status():
     status_file = os.path.join(app.root_path, 'status_monitors.yaml')
     with open(status_file, 'r') as f:
         config = yaml.safe_load(f)
+    
+    for page in config['status_pages']:
+        try:
+            response = requests.get(
+                f"{config['uptime_kuma_url']}/api/status-page/{page['slug']}",
+                timeout=5
+            )
+            if response.status_code == 200:
+                data = response.json()
+                page['status'] = 'UP'
+                if 'publicGroupList' in data:
+                    for group in data['publicGroupList']:
+                        for monitor in group.get('monitorList', []):
+                            if monitor.get('status') != 1:
+                                page['status'] = 'DOWN'
+                                break
+            else:
+                page['status'] = 'UNKNOWN'
+        except:
+            page['status'] = 'UNKNOWN'
+    
+    config['last_check'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S MST')
     
     return render_template('status.html', config=config)
 
